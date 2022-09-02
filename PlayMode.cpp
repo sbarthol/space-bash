42e56c84112611f7b08bb38ea94ae923574cd0b0
Sacha Bartholme <sacha.bartholme@hotmail.com>
2022-09-02 17:27:40 -0400

    format

--- PlayMode.cpp
+++ PlayMode.cpp
@@ -15,20 +15,18 @@ PlayMode::PlayMode() {
   load_png_tu_ppu();
   for (uint32_t i = 0; i < PPU466::BackgroundWidth * PPU466::BackgroundHeight;
        i++) {
+    uint32_t k = gen32();
+    constexpr uint32_t StarPeriod = 100;
 
-        uint32_t k = gen32();
-        constexpr uint32_t StarPeriod = 100;
-
-        if(k%StarPeriod == 0) {
-            ppu.background[i] = (tile_idx_to_palette_idx[18] << 8) | 18;
-        }else if(k%StarPeriod==1) {
-            ppu.background[i] = (tile_idx_to_palette_idx[19] << 8) | 19;
-        }else if(k%StarPeriod==2) {
-            ppu.background[i] = (tile_idx_to_palette_idx[20] << 8) | 20;
-        }else {
-            ppu.background[i] = 255;
-        }
-    
+    if (k % StarPeriod == 0) {
+      ppu.background[i] = (tile_idx_to_palette_idx[18] << 8) | 18;
+    } else if (k % StarPeriod == 1) {
+      ppu.background[i] = (tile_idx_to_palette_idx[19] << 8) | 19;
+    } else if (k % StarPeriod == 2) {
+      ppu.background[i] = (tile_idx_to_palette_idx[20] << 8) | 20;
+    } else {
+      ppu.background[i] = 255;
+    }
   }
   player_at.x = PPU466::ScreenWidth / 2 - 8;
   player_at.y = PPU466::ScreenHeight / 2 - 16;
@@ -77,37 +75,38 @@ bool PlayMode::handle_event(SDL_Event const &evt, glm::uvec2 const &window_size)
 }
 
 void PlayMode::update(float elapsed) {
-
-  if(has_lost){
+  if (has_lost) {
     return;
   }
 
-	constexpr float PlayerSpeed = 150.0f;
-	if (left.pressed) player_at.x -= PlayerSpeed * elapsed;
-	if (right.pressed) player_at.x += PlayerSpeed * elapsed;
-	if (down.pressed) player_at.y -= PlayerSpeed * elapsed;
-	if (up.pressed) player_at.y += PlayerSpeed * elapsed;
+  constexpr float PlayerSpeed = 150.0f;
+  if (left.pressed) player_at.x -= PlayerSpeed * elapsed;
+  if (right.pressed) player_at.x += PlayerSpeed * elapsed;
+  if (down.pressed) player_at.y -= PlayerSpeed * elapsed;
+  if (up.pressed) player_at.y += PlayerSpeed * elapsed;
 
-  for(const Projectile &p: projectiles) {
-    bool width_positive = std::min(player_at.x + 16, p.pos.x + 8) > std::max(player_at.x, p.pos.x);
-    bool height_positive = std::min(player_at.y + 32, p.pos.y + 8) > std::max(player_at.y, p.pos.y);
+  for (const Projectile &p : projectiles) {
+    bool width_positive = std::min(player_at.x + 16, p.pos.x + 8) >
+                          std::max(player_at.x, p.pos.x);
+    bool height_positive = std::min(player_at.y + 32, p.pos.y + 8) >
+                           std::max(player_at.y, p.pos.y);
     has_lost = width_positive && height_positive;
-    if(has_lost) {
+    if (has_lost) {
       break;
     }
   }
 
-	//reset button press counters:
-	left.downs = 0;
-	right.downs = 0;
-	up.downs = 0;
-	down.downs = 0;
+  // reset button press counters:
+  left.downs = 0;
+  right.downs = 0;
+  up.downs = 0;
+  down.downs = 0;
 
   constexpr float ProjectileSpeed = 80.0f;
-  static const int dx[] = {0,1,0,-1};
-  static const int dy[] = {1,0,-1,0};
+  static const int dx[] = {0, 1, 0, -1};
+  static const int dy[] = {1, 0, -1, 0};
 
-  for(Projectile &p: projectiles) {
+  for (Projectile &p : projectiles) {
     p.pos.x += dx[p.dir] * ProjectileSpeed * elapsed;
     p.pos.y += dy[p.dir] * ProjectileSpeed * elapsed;
   }
@@ -116,41 +115,40 @@ void PlayMode::update(float elapsed) {
   constexpr float NewProjectilePeriod = 0.3f;
 
   if (total_elapsed > NewProjectilePeriod) {
-
     total_elapsed -= NewProjectilePeriod;
 
-    uint32_t tile_idx = gen32()%4+2;
+    uint32_t tile_idx = gen32() % 4 + 2;
     uint8_t dir = gen32() % 4;
     glm::vec2 pos;
-    switch (dir)
-    {
-    case 0:
-      pos.x = gen32() % (PPU466::ScreenWidth -7);
-      pos.y = 0;
-      break;
-    case 1:
-      pos.x = 0;
-      pos.y = gen32() % (PPU466::ScreenHeight -7);
-      break;
-    case 2:
-      pos.x = gen32() % (PPU466::ScreenWidth -7);
-      pos.y = PPU466::ScreenHeight - 8;
-      break;
-    case 3:
-      pos.x = PPU466::ScreenWidth - 8;
-      pos.y = gen32() % (PPU466::ScreenHeight -7);
-      break;
-    default:
-      break;
+    switch (dir) {
+      case 0:
+        pos.x = gen32() % (PPU466::ScreenWidth - 7);
+        pos.y = 0;
+        break;
+      case 1:
+        pos.x = 0;
+        pos.y = gen32() % (PPU466::ScreenHeight - 7);
+        break;
+      case 2:
+        pos.x = gen32() % (PPU466::ScreenWidth - 7);
+        pos.y = PPU466::ScreenHeight - 8;
+        break;
+      case 3:
+        pos.x = PPU466::ScreenWidth - 8;
+        pos.y = gen32() % (PPU466::ScreenHeight - 7);
+        break;
+      default:
+        break;
     }
     projectiles.push_front({.tile_idx = tile_idx, .pos = pos, .dir = dir});
   }
 
-  while(projectiles.size() > 0) {
+  while (projectiles.size() > 0) {
     Projectile back = projectiles.back();
-    if(0 <= back.pos.x && back.pos.x < PPU466::ScreenWidth && 0 <= back.pos.y && back.pos.y < PPU466::ScreenHeight) {
+    if (0 <= back.pos.x && back.pos.x < PPU466::ScreenWidth &&
+        0 <= back.pos.y && back.pos.y < PPU466::ScreenHeight) {
       break;
-    }else{
+    } else {
       projectiles.pop_back();
     }
   }
@@ -159,53 +157,53 @@ void PlayMode::update(float elapsed) {
 }
 
 void PlayMode::draw(glm::uvec2 const &drawable_size) {
-	//--- set ppu state based on game state ---	
+  //--- set ppu state based on game state ---
 
-	//rocket sprite:
-	ppu.sprites[0].x = int8_t(player_at.x);
-	ppu.sprites[0].y = int8_t(player_at.y);
-	ppu.sprites[0].index = 48;
-	ppu.sprites[0].attributes = tile_idx_to_palette_idx[48];
+  // rocket sprite:
+  ppu.sprites[0].x = int8_t(player_at.x);
+  ppu.sprites[0].y = int8_t(player_at.y);
+  ppu.sprites[0].index = 48;
+  ppu.sprites[0].attributes = tile_idx_to_palette_idx[48];
 
-	ppu.sprites[1].x = int8_t(player_at.x) + 8;
-	ppu.sprites[1].y = int8_t(player_at.y);
-	ppu.sprites[1].index = 49;
-	ppu.sprites[1].attributes = tile_idx_to_palette_idx[49];
+  ppu.sprites[1].x = int8_t(player_at.x) + 8;
+  ppu.sprites[1].y = int8_t(player_at.y);
+  ppu.sprites[1].index = 49;
+  ppu.sprites[1].attributes = tile_idx_to_palette_idx[49];
 
-	ppu.sprites[2].x = int8_t(player_at.x);
-	ppu.sprites[2].y = int8_t(player_at.y) + 8;
-	ppu.sprites[2].index = 32;
-	ppu.sprites[2].attributes = tile_idx_to_palette_idx[32];
+  ppu.sprites[2].x = int8_t(player_at.x);
+  ppu.sprites[2].y = int8_t(player_at.y) + 8;
+  ppu.sprites[2].index = 32;
+  ppu.sprites[2].attributes = tile_idx_to_palette_idx[32];
 
-	ppu.sprites[3].x = int8_t(player_at.x) + 8;
-	ppu.sprites[3].y = int8_t(player_at.y) + 8;
-	ppu.sprites[3].index = 33;
-	ppu.sprites[3].attributes = tile_idx_to_palette_idx[33];
+  ppu.sprites[3].x = int8_t(player_at.x) + 8;
+  ppu.sprites[3].y = int8_t(player_at.y) + 8;
+  ppu.sprites[3].index = 33;
+  ppu.sprites[3].attributes = tile_idx_to_palette_idx[33];
 
-	ppu.sprites[4].x = int8_t(player_at.x);
-	ppu.sprites[4].y = int8_t(player_at.y) + 16;
-	ppu.sprites[4].index = 16;
-	ppu.sprites[4].attributes = tile_idx_to_palette_idx[16];
+  ppu.sprites[4].x = int8_t(player_at.x);
+  ppu.sprites[4].y = int8_t(player_at.y) + 16;
+  ppu.sprites[4].index = 16;
+  ppu.sprites[4].attributes = tile_idx_to_palette_idx[16];
 
-	ppu.sprites[5].x = int8_t(player_at.x) + 8;
-	ppu.sprites[5].y = int8_t(player_at.y) + 16;
-	ppu.sprites[5].index = 17;
-	ppu.sprites[5].attributes = tile_idx_to_palette_idx[17];
+  ppu.sprites[5].x = int8_t(player_at.x) + 8;
+  ppu.sprites[5].y = int8_t(player_at.y) + 16;
+  ppu.sprites[5].index = 17;
+  ppu.sprites[5].attributes = tile_idx_to_palette_idx[17];
 
-	ppu.sprites[6].x = int8_t(player_at.x);
-	ppu.sprites[6].y = int8_t(player_at.y) + 24;
-	ppu.sprites[6].index = 0;
-	ppu.sprites[6].attributes = tile_idx_to_palette_idx[0];
+  ppu.sprites[6].x = int8_t(player_at.x);
+  ppu.sprites[6].y = int8_t(player_at.y) + 24;
+  ppu.sprites[6].index = 0;
+  ppu.sprites[6].attributes = tile_idx_to_palette_idx[0];
 
-	ppu.sprites[7].x = int8_t(player_at.x) + 8;
-	ppu.sprites[7].y = int8_t(player_at.y) + 24;
-	ppu.sprites[7].index = 1;
-	ppu.sprites[7].attributes = tile_idx_to_palette_idx[1];
+  ppu.sprites[7].x = int8_t(player_at.x) + 8;
+  ppu.sprites[7].y = int8_t(player_at.y) + 24;
+  ppu.sprites[7].index = 1;
+  ppu.sprites[7].attributes = tile_idx_to_palette_idx[1];
 
   uint32_t sprite_idx = 8;
 
   // projectile sprites
-  for(Projectile &p: projectiles) {
+  for (Projectile &p : projectiles) {
     ppu.sprites[sprite_idx].x = p.pos.x;
     ppu.sprites[sprite_idx].y = p.pos.y;
     ppu.sprites[sprite_idx].index = p.tile_idx;
@@ -213,17 +211,17 @@ void PlayMode::draw(glm::uvec2 const &drawable_size) {
     sprite_idx++;
   }
 
-  //background scroll:
-	ppu.background_position.x = int32_t(-0.2f * player_at.x);
-	ppu.background_position.y = int32_t(-0.2f * player_at.y);
+  // background scroll:
+  ppu.background_position.x = int32_t(-0.2f * player_at.x);
+  ppu.background_position.y = int32_t(-0.2f * player_at.y);
 
-	// the rest of the sprites are not visible
-	for (; sprite_idx < 64; sprite_idx++) {
-		ppu.sprites[sprite_idx].y = 240;
-	}
+  // the rest of the sprites are not visible
+  for (; sprite_idx < 64; sprite_idx++) {
+    ppu.sprites[sprite_idx].y = 240;
+  }
 
-	//--- actually draw ---
-	ppu.draw(drawable_size);
+  //--- actually draw ---
+  ppu.draw(drawable_size);
 }
 
 void PlayMode::load_png_tu_ppu() {
@@ -240,9 +238,10 @@ void PlayMode::load_png_tu_ppu() {
           png_bytep px = &(row[x * 4]);
 
           glm::u8vec4 color(px[0], px[1], px[2], px[3]);
-          if(std::find(current_palette.begin(), current_palette.end(), color) == current_palette.end()){
-						current_palette.push_back(color);
-					}
+          if (std::find(current_palette.begin(), current_palette.end(),
+                        color) == current_palette.end()) {
+            current_palette.push_back(color);
+          }
         }
       }
 
@@ -251,8 +250,8 @@ void PlayMode::load_png_tu_ppu() {
       }
 
       auto lt = [](glm::u8vec4 a, glm::u8vec4 b) {
-        return (a[0] | (a[1] << 8) | (a[2] << 16) | (a[3] << 24)) < (b[0] |
-               (b[1] << 8) | (b[2] << 16) | (b[3] << 24));
+        return (a[0] | (a[1] << 8) | (a[2] << 16) | (a[3] << 24)) <
+               (b[0] | (b[1] << 8) | (b[2] << 16) | (b[3] << 24));
       };
 
       sort(current_palette.begin(), current_palette.end(), lt);
@@ -278,7 +277,7 @@ void PlayMode::load_png_tu_ppu() {
         throw std::runtime_error("Need more than 8 palettes");
       }
 
-			tile_idx_to_palette_idx[i * 16 + j] = palette_idx;
+      tile_idx_to_palette_idx[i * 16 + j] = palette_idx;
     }
   }
 
@@ -287,13 +286,12 @@ void PlayMode::load_png_tu_ppu() {
       std::vector<glm::u8vec4> current_palette =
           palette_table[tile_idx_to_palette_idx[i * 16 + j]];
       PPU466::Tile current_tile;
-			current_tile.bit0.fill(0);
-			current_tile.bit1.fill(0);
+      current_tile.bit0.fill(0);
+      current_tile.bit1.fill(0);
 
       for (uint32_t y = i * 8; y < i * 8 + 8; y++) {
         png_bytep row = row_pointers[y];
         for (uint32_t x = j * 8; x < j * 8 + 8; x++) {
-					
           png_bytep px = &(row[x * 4]);
 
           glm::u8vec4 color(px[0], px[1], px[2], px[3]);
@@ -303,9 +301,8 @@ void PlayMode::load_png_tu_ppu() {
           assert(it != current_palette.end());
           uint32_t color_idx = it - current_palette.begin();
 
-					current_tile.bit0[7 - y + i * 8] |= (color_idx & 1) << (x - j * 8);
+          current_tile.bit0[7 - y + i * 8] |= (color_idx & 1) << (x - j * 8);
           current_tile.bit1[7 - y + i * 8] |= (color_idx >> 1) << (x - j * 8);
-          
         }
       }
       tile_table[i * 16 + j] = current_tile;
@@ -328,8 +325,12 @@ void PlayMode::load_png_tu_ppu() {
 }
 
 void PlayMode::read_png_file(std::string filename) {
-  // http://www.libpng.org/pub/png/book/chapter13.html
-  // https://gist.github.com/jeroen/10eb17a9fb0e5799b772
+  /*
+  For following code I took inspiration from
+  http://www.libpng.org/pub/png/book/chapter13.html and
+  https://gist.github.com/jeroen/10eb17a9fb0e5799b772
+  */
+
   FILE *fp = fopen(filename.c_str(), "rb");
 
   png_structp png =
